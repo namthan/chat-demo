034980edbd7f3de808840cfb99d2cab065f81d53
namthan <namtm.it@gmail.com>
2021-07-19 08:12:35 +0700

    Add mongoose, socketio

--- src/app.js
+++ src/app.js
@@ -1,31 +1,57 @@
 import express, { request } from "express";
-var flash = require("connect-flash");
 import { nanoid } from "nanoid";
 
 import { urlencoded, json } from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import { pgFactory } from "./PGFactory";
-import { dataDao } from "./dao/DataDao";
-import { pushNotification } from "./dao/PushNotification";
-import { smssend } from "./SMSSend";
+import http from 'http';
+
+var mongoose = require('mongoose');
 
 const app = express();
-dotenv.config();
-
-app.use(flash());
 app.use(urlencoded({ extended: true, limit: "500mb" }));
 app.use(json({ extended: true, limit: "500mb" }));
 app.use(cors());
-let server = app.listen(process.env.PORT);
+
+dotenv.config();
+var io = require('socket.io')(server);
+
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+
+const dbUrl = 'mongodb://localhost:27017/chat'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+
+mongoose.connect(dbUrl, { useUnifiedTopology: true, useNewUrlParser: true })
+
+var server = app.listen(process.env.PORT);
 
 server.setTimeout(500000);
 
 //Use body-parser
 app.use(urlencoded({ extended: false }));
-
-app.use((req, res, next) => {
-  
-});
 
